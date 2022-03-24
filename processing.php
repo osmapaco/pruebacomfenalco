cda5ac472d4d9dcfbc18335cb8d29ae1e42caca2
osmapaco <84645054+osmapaco@users.noreply.github.com>
2022-03-24 15:06:50 -0500

    Add files via upload

---
+++ processing.php
@@ -0,0 +1,5 @@
+<form method="post" action="processing.php">
+First name: <input type="text" name="firstname"><br>
+Last name: <input type="text" name="lastname"><br>
+<input type="submit" value="Submit">
+</form>
