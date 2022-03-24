c3615cb450705b11b0f9eee014405f166127beef
osmapaco <84645054+osmapaco@users.noreply.github.com>
2022-03-24 15:16:01 -0500

    Delete processing.php

--- processing.php
+++
@@ -1,5 +0,0 @@
-<form method="post" action="processing.php">
-First name: <input type="text" name="firstname"><br>
-Last name: <input type="text" name="lastname"><br>
-<input type="submit" value="Submit">
-</form>
