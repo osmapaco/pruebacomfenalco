acca10c64b293559129821046a5ae69283dda3f2
osmapaco <84645054+osmapaco@users.noreply.github.com>
2022-03-24 15:56:16 -0500

    Add files via upload

---
+++ processing.php
@@ -0,0 +1,12 @@
+<html>
+    <head></head>
+    <body>
+     <?php
+       $firstname= $_POST['firstname'];
+       $lastname= $_POST['lastname'];
+       echo $firstname;
+       echo $lastname;
+      ?>
+      Hola todos
+    </body>
+</html>
